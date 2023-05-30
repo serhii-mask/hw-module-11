4df8011506d616036039632f994c9ee4aad47a6b
Serhii <serhii.mask@gmail.com>
2023-05-30 20:53:40 +0300

    feat: add task1

---
+++ src/main/java/task1/PrintName.java
@@ -0,0 +1,6 @@
+package task1;
+
+@FunctionalInterface
+public interface PrintName {
+    String print(String[] names);
+}
---
+++ src/main/java/task1/PrintOddNameTask.java
@@ -0,0 +1,23 @@
+package task1;
+
+public class PrintOddNameTask {
+    public static void main(String[] args) {
+        String[] names = new String[]{"Ivan", "Oleksandra", "Petro", "Leonid", "Maks", "Masha"};
+
+        PrintName printOddName = (array) -> {
+            String result = "";
+            for (int i = 0; i < array.length; i++) {
+                if (i % 2 != 0) {
+                    result += i + ". " + array[i];
+                }
+
+                if (i % 2 != 0 && i < array.length - 1) {
+                    result += ", ";
+                }
+            }
+            return result;
+        };
+
+        System.out.println("printOddName = " + printOddName.print(names));
+    }
+}
