1bdd4ccecf6086c0b92eea54c01d05eff126d8de
Serhii <serhii.mask@gmail.com>
2023-06-01 21:32:08 +0300

    fix: task2 improve code

--- src/main/java/task2/SortReverseToUpperCasePrintNames.java
+++ src/main/java/task2/SortReverseToUpperCasePrintNames.java
@@ -1,19 +1,18 @@
 package task2;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SortReverseToUpperCasePrintNames {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Ivan", "Oleksandra", "Petro", "Leonid",
                 "Maks", "Masha", "Andriy", "Orest", "Anna", "Viktoria");
 
-        List<String> reverseNamesToUpperCase = new ArrayList<>();
-
-        names.stream()
+        List<String> reverseNamesToUpperCase = names.stream()
+                .map(String::toUpperCase)
                 .sorted()
-                .forEach(n -> reverseNamesToUpperCase.add(0, n.toUpperCase()));
+                .collect(Collectors.toList());
 
         System.out.println("newNames = " + reverseNamesToUpperCase);
         System.out.println("listStream = " + names);
