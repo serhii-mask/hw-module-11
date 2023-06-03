3e578dabc193a3b0e7eb7d9e5678bd49f2220a5b
Serhii <serhii.mask@gmail.com>
2023-06-03 19:30:39 +0300

    fix: task2 improve code

--- src/main/java/task2/SortReverseToUpperCasePrintNames.java
+++ src/main/java/task2/SortReverseToUpperCasePrintNames.java
@@ -1,10 +1,8 @@
 package task2;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SortReverseToUpperCasePrintNames {
     public static void main(String[] args) {
